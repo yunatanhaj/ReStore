40089c46fc9bbfc6510eeabc50abd2e40c7c0f5c
yunatan haj <yunatanhaj1@gmail.com>
2023-12-01 18:08:57 +0200

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,7 +23,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+});
+app.UseAuthentication();
 app.MapControllers();
 
 var scope  = app.Services.CreateScope();
---
+++ client/src/features/catalog/ProductList.tsx
@@ -0,0 +1,22 @@
+import { Grid } from "@mui/material";
+import { Product } from "../../app/models/product";
+import ProductCard from "./ProductCard";
+
+interface Props {
+
+    products: Product[];
+}
+
+export default function ProductList({products} : Props){
+    return (
+    <Grid container spacing={4}>
+        {products.map(product => (
+            <Grid key={product.id} item xs={3}>
+                <ProductCard  product={product}/>
+            </Grid>
+          
+        ))}
+      </Grid>
+      )
+
+}
